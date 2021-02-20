727f5831877aa332c27da27304c7f14574ff319b
Surafel Asfaw <surafel.asfaw24@gmail.com>
2021-02-20 13:17:44 -0500

    adds more test coverage for dependent methods

--- src/test/java/com/collabera/hcp/controller/EnrolleeControllerTest.java
+++ src/test/java/com/collabera/hcp/controller/EnrolleeControllerTest.java
@@ -1,5 +1,6 @@
 package com.collabera.hcp.controller;
 
+import com.collabera.hcp.model.Dependent;
 import com.collabera.hcp.model.Enrollee;
 import com.collabera.hcp.service.IEnrolleeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,9 +13,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
 import java.util.Date;
 
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -80,6 +81,90 @@ public class EnrolleeControllerTest {
 
         mockMVC
                 .perform(delete("/api/enrollee/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void post_ShouldAddDependent() throws Exception {
+        Dependent dependent = Dependent.builder()
+                .id(2)
+                .name("kirubel asfaw")
+                .birthDate(new Date())
+                .build();
+
+        Enrollee enrollee = Enrollee.builder()
+                .id(1)
+                .name("surafel asfaw")
+                .birthDate(new Date())
+                .phoneNumber("240-418-1349")
+                .status(true)
+                .dependents(Arrays.asList(dependent))
+                .build();
+
+        String dependentJSON = objectMapper.writeValueAsString(dependent);
+        String enrolleeJSON = objectMapper.writeValueAsString(enrollee);
+
+        when(enrolleeService.addDependent(Mockito.any(Dependent.class), Mockito.any(Integer.class)))
+                .thenReturn(enrollee);
+
+        String result = mockMVC.perform(post("/api/enrollee/1/dependent")
+                .content(dependentJSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JSONAssert.assertEquals(enrolleeJSON, result, false);
+    }
+
+    @Test
+    public void put_ShouldUpdateDependent() throws Exception {
+        Dependent dependent = Dependent.builder()
+                .id(2)
+                .name("kirubel asfaw")
+                .birthDate(new Date())
+                .build();
+
+        Enrollee enrollee = Enrollee.builder()
+                .id(1)
+                .name("surafel asfaw")
+                .birthDate(new Date())
+                .phoneNumber("240-418-1349")
+                .status(true)
+                .dependents(Arrays.asList(dependent))
+                .build();
+
+        String dependentJSON = objectMapper.writeValueAsString(dependent);
+
+        when(enrolleeService.modifyEnrollee(Mockito.any(Enrollee.class)))
+                .thenReturn(enrollee);
+
+        mockMVC.perform(put("/api/enrollee/1/dependent")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(dependentJSON))
+                .andExpect(status().isOk());
+
+        Dependent dependent2 = Dependent.builder()
+                .id(2)
+                .birthDate(new Date())
+                .build();
+
+        String dependentJSON2 = objectMapper.writeValueAsString(dependent2);
+
+        mockMVC.perform(put("/api/enrollee/1/dependent")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(dependentJSON2))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void delete_ShouldDeleteDependent() throws Exception {
+        when(enrolleeService.removeEnrollee(Mockito.any(Integer.class)))
+                .thenReturn(1);
+
+        mockMVC.perform(delete("/api/enrollee/1")
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 }
--- src/test/java/com/collabera/hcp/service/EnrolleeServiceTest.java
+++ src/test/java/com/collabera/hcp/service/EnrolleeServiceTest.java
@@ -1,5 +1,6 @@
 package com.collabera.hcp.service;
 
+import com.collabera.hcp.model.Dependent;
 import com.collabera.hcp.model.Enrollee;
 import com.collabera.hcp.repository.IEnrolleeRepository;
 import com.collabera.hcp.service.impl.EnrolleeService;
@@ -20,7 +21,6 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EnrolleeServiceTest {
-
     @Mock
     private IEnrolleeRepository enrolleeRepository;
 
@@ -80,4 +80,81 @@ class EnrolleeServiceTest {
         verify(enrolleeRepository, times(1)).deleteById(Mockito.any(Integer.class));
     }
 
+    @Test
+    public void saveDependent_shouldReturn() {
+        Dependent dependent = Dependent.builder()
+                .name("kirubel asfaw")
+                .birthDate(new Date())
+                .build();
+
+        Enrollee enrollee = Enrollee.builder()
+                .id(1)
+                .name("surafel asfaw")
+                .birthDate(new Date())
+                .phoneNumber("240-418-1349")
+                .status(true)
+                .dependents(new ArrayList<>())
+                .build();
+
+        when(enrolleeRepository.save(any(Enrollee.class)))
+                .thenReturn(enrollee);
+        when(enrolleeRepository.findById(any(Integer.class)))
+                .thenReturn(Optional.of(enrollee));
+
+        Enrollee actual = enrolleeService.addDependent(dependent, 1);
+
+        assertThat(actual.getDependents().size()).isEqualTo(1);
+        assertThat(actual.getDependents().get(0)).isEqualTo(dependent);
+    }
+
+    @Test
+    public void updateDependent_shouldReturn() {
+        Dependent dependent = Dependent.builder()
+                .id(2)
+                .name("kirubel asfaw")
+                .birthDate(new Date())
+                .build();
+
+        Enrollee enrollee = Enrollee.builder()
+                .name("surafel asfaw")
+                .birthDate(new Date())
+                .phoneNumber("240-418-1349")
+                .status(true)
+                .dependents(Arrays.asList(Dependent.builder()
+                        .id(2)
+                        .name("asfaw asfaw")
+                        .birthDate(new Date())
+                        .build()))
+                .build();
+
+        when(enrolleeRepository.findById(Mockito.any(Integer.class)))
+                .thenReturn(Optional.of(enrollee));
+        when(enrolleeRepository.save(Mockito.any(Enrollee.class)))
+                .thenReturn(enrollee);
+
+        Enrollee actual = enrolleeService.modifyDependent(dependent, 1);
+
+        verify(enrolleeRepository, times(1)).save(Mockito.any(Enrollee.class));
+        assertThat(actual.getDependents().size()).isEqualTo(1);
+        assertThat(actual.getDependents().get(0).getName()).isEqualTo("kirubel asfaw");
+    }
+
+    @Test
+    public void deleteDependent_shouldDelete() {
+        Enrollee enrollee = Enrollee.builder()
+                .name("surafel asfaw")
+                .birthDate(new Date())
+                .phoneNumber("240-418-1349")
+                .status(true)
+                .build();
+
+        doNothing().when(enrolleeRepository).deleteById(Mockito.any(Integer.class));
+
+        when(enrolleeRepository.findById(Mockito.any(Integer.class)))
+                .thenReturn(Optional.of(enrollee));
+
+        enrolleeService.removeEnrollee(1);
+
+        verify(enrolleeRepository, times(1)).deleteById(Mockito.any(Integer.class));
+    }
 }
