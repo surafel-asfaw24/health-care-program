0919c3cd6db7b7f248e32f62d57d0be14c7f8c5b
Surafel Asfaw <surafel.asfaw24@gmail.com>
2021-02-19 20:19:07 -0500

    initial setup

---
+++ src/main/java/com/collabera/hcp/HcpApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.hcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/collabera/hcp/HcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabera.hcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
